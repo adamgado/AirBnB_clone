bb0ebcf742b16b6af79e2d6b1caa83238dd4f133
adamgado <adamgado33@gmail.com>
2024-02-09 07:19:40 -0800

    console 0.2 show function not working properly

--- console.py
+++ console.py
@@ -42,6 +42,25 @@ class HBNBCommand(cmd.Cmd):
             created.save()
             print(created.id)
 
+    def do_show(self, *args):
+        """print string form of object"""
+        class_name = args[0]
+        obj_id = ''
+        if len(args) > 1:
+            obj_id = args[1]
+        if class_name is None:
+            print('** class name missing **')
+        elif class_name not in self.class_list:
+            print("** class doesn't exist **")
+        elif obj_id == '':
+            print('** instance id missing **')
+        else:
+            found_obj = models.storage.all().get(class_name + '.' + obj_id)
+            if found_obj == '':
+                print('** no instance found **')
+            else:
+                print(found_obj)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
