bcfd0ceb938f841a226bec093d0cb09b5c2b7b68
adamgado <65188827+adamgado@users.noreply.github.com>
2024-02-11 16:46:41 +0200

    Revert "more basemodel fixes"
    This reverts commit b4f1f34decb534db3b8a2028409d7263ff11d3df.

--- models/base_model.py
+++ models/base_model.py
@@ -16,11 +16,6 @@ class BaseModel():
                     b = datetime.strptime(b, '%Y-%m-%dT%H:%M:%S.%f')
                 if a != '__class__':
                     setattr(self, a, b)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            models.storage.new(self)
 
     def __str__(self):
         """return str representation of object"""
