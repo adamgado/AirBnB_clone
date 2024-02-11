0439aa8687b12df7a96aa8fc7cb2ecb626867c2f
adamgado <65188827+adamgado@users.noreply.github.com>
2024-02-11 16:59:18 +0200

    extra fixes

--- console.py
+++ console.py
@@ -96,6 +96,7 @@ class HBNBCommand(cmd.Cmd):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     def do_update(self, arg):
         """update object attributes"""
@@ -129,6 +130,8 @@ class HBNBCommand(cmd.Cmd):
                 found_obj.save()
 
 >>>>>>> parent of 2ea67b9 (console 0.7.1, more fixes + basemodel fix)
+=======
+>>>>>>> parent of 56755d6 (console 0.6, update)
 =======
 >>>>>>> parent of 56755d6 (console 0.6, update)
 =======
